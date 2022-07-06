e36fdc765938782bdd1bc6191616e3cd257ef469
TuanDinh <dinhngoctuan2131997@gmail.com>
2022-07-06 14:27:19 +0300

    moduls

--- convert_moduls.py/app.py
+++ convert_moduls.py/app.py
@@ -1,6 +1,9 @@
 import converted
 from converted import kg_to_lbs
 from converted import lbs_to_kg
-
+from converted import find_max
 print(converted.kg_to_lbs(25))
 print(kg_to_lbs(25))
+
+numbers = [1,2,5,10]
+print(f"Max of array = {find_max(numbers= numbers)}")
--- convert_moduls.py/converted.py
+++ convert_moduls.py/converted.py
@@ -3,3 +3,12 @@ def lbs_to_kg(weigth):
 
 def kg_to_lbs(weight):
     return weight/0.45
+
+def find_max(numbers):
+    max = numbers[0]
+    for number in numbers:
+        if max < number:
+            max = number
+    return max
+
+
