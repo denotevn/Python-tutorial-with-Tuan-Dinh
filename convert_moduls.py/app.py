d0088b1a9d2e423b16f4bab90a82c2cbd882668f
TuanDinh <dinhngoctuan2131997@gmail.com>
2022-07-06 14:22:40 +0300

    moduls

---
+++ convert_moduls.py/app.py
@@ -0,0 +1,6 @@
+import converted
+from converted import kg_to_lbs
+from converted import lbs_to_kg
+
+print(converted.kg_to_lbs(25))
+print(kg_to_lbs(25))
---
+++ convert_moduls.py/converted.py
@@ -0,0 +1,5 @@
+def lbs_to_kg(weigth):
+    return weigth * 0.45
+
+def kg_to_lbs(weight):
+    return weight/0.45
